7d4c00245c3be9b5fe250cf4b18423a42e9663c3
Lenoch <81lennoch@gmail.com>
2024-11-07 18:48:13 +0100

    Fix route handling by disabling redirect_slashes
    - Disabled `redirect_slashes` in FastAPI app initialization.
    - Added routes without trailing slashes to ensure proper request handling.

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ if 'database-path' not in config or not os.path.exists(config['database-path']):
     print("Error: 'database-path' is not set or does not exist.")
     sys.exit(1)
 
-app = FastAPI(redirect_slashes=True)
+app = FastAPI(redirect_slashes=False)
 
 # In-memory store for request counts
 request_counts = defaultdict(list)
@@ -240,8 +240,11 @@ def format_json_dates(data: dict) -> dict:
     return data
 
 
+@app.get('/{platform}/{tid}')
 @app.get('/{platform}/{tid}/')
+@app.get('/{platform}/{tid}/{asset_type}')
 @app.get('/{platform}/{tid}/{asset_type}/')
+@app.get('/{platform}/{tid}/{asset_type}/{screen_id}')
 @app.get('/{platform}/{tid}/{asset_type}/{screen_id}/')
 async def get_nx(platform: str, tid: str, asset_type: str = None, screen_id = 1):
     if platform.lower() not in ['nx', 'switch']:
