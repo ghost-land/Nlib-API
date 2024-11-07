59f3080b5db0d41d5beed96820da53692c3bfbf3
Lenoch <81lennoch@gmail.com>
2024-11-07 18:31:54 +0100

    Use `source` to activate virtual environment
    Replaced the dot command (.) with the more explicit `source` command for activating the virtual environment.

--- venv.sh
+++ venv.sh
@@ -4,12 +4,12 @@
 if [ ! -d ".venv" ]; then
     echo "Creating virtual environment..."
     sudo python3 -m venv .venv
-    . .venv/bin/activate
+    source .venv/bin/activate
     python3 -m pip install -r requirements.txt
     echo "Virtual environment created successfully."
 else
     echo "Virtual environment already exists. Activating it..."
-    . .venv/bin/activate
+    source .venv/bin/activate
     echo "Installing requirements..."
     python3 -m pip install -r requirements.txt
 fi
